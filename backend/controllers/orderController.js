8c552707753a3d10d33345796ff6c5a8129eeaaa
AswinAK1 <aswinak1212@gmail.com>
2025-01-29 16:02:48 +0530

    url added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order from frontend
 const placeOrder = async(req,res) =>{
 
-  const frontend_url = "http://localhost:3000"
+  const frontend_url = "https://food-delivery-app-xb5k.onrender.com"
 
   try {
     const newOrder = new orderModel({
